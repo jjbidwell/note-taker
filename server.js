bbbe83a46593785bf604401c07c9b2743f0a9dc2
Joshua Bidwell <jjbidwell262@gmail.com>
2020-10-08 17:46:05 -0700

    added post route for api/characters

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 4400;
 
+let notes = [];
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -19,6 +21,15 @@ app.get('/api/notes', (req, res) => {
     res.end('<h1>API note Page</h1>');
 })
 
+
+app.post("/api/characters", (req, res) => {
+    const newNote = req.body;
+  
+    console.log(newNote);
+    notes.push(newNote);
+    res.json(notes);
+
+  });
 
 app.listen(PORT, () => {
     console.log("App listening on PORT " + PORT);
