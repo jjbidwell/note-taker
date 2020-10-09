de40df4b66dc278495b388d13c2a15254e47f7bc
Joshua Bidwell <jjbidwell262@gmail.com>
2020-10-08 21:23:06 -0700

    Made it so list comes from json file. Submit also saves to the notes.json file

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
 const app = express();
 const PORT = process.env.PORT || 4400;
@@ -10,37 +11,38 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
-})
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 })
 
 app.get('/api/notes', (req, res) => {
-    return res.json(notes);
+    res.sendFile(path.join(__dirname, "/public/db/notes.json"));
 })
 
 app.get('/api/notes/:id', (req, res) => {
     const chosen = req.params.id;
-    console.log(chosen);
-  
+
     for (let i = 0; i < notes.length; i++) {
       if (chosen === notes[i].id) {
         return res.json(notes[i]);
       }
     }
-  
-    return res.json(false);
 });
+
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+})
+
 
 
 app.post("/api/notes", (req, res) => {
     const newNote = req.body;
-  
-    console.log(newNote);
     notes.push(newNote);
+    fs.writeFile(path.join(__dirname, "/public/db/notes.json") , JSON.stringify(notes, null, 5), (err) => {
+        if (err) throw err;
+    })
     res.json(notes);
 
   });
@@ -57,6 +59,8 @@ app.post("/api/notes", (req, res) => {
         };
     });
   });
+
+
 
 app.listen(PORT, () => {
     console.log("App listening on PORT " + PORT);
